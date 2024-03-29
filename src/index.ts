6d3326b4d8e8422fddef2e7046941f0f713b3525
Sid <siddharth@nukon.com>
2024-03-29 18:21:45 +0530

    init updated

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface Color {
+    red: number;
+    blue: number;
+    yellow: number;
+}
+
+const test: Color = {
+    red: 1,
+    blue: 2,
+    yellow: 3
+
+};
+
+console.log(test);
+
+export default test;
