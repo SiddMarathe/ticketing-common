451d86e2e0310110376785564a349dc00d05fffb
Sid <siddharth@nukon.com>
2024-04-04 09:52:12 +0530

    updated

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import UnauthorizedError from '../errors/unauthorized-error';
 
 const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+
     if ( ! req.currentUser ) {
         throw new UnauthorizedError();
     }
+
+    console.log('Authorized');
+    next();
 };
 
 export default requireAuth;
